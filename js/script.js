631970b5c90a5586f9edfe271b88f7d695d307d3
Ivan Kopytin <i.ivanzemsky2005@gmail.com>
2022-10-11 21:18:37 +0300

    faq slider start

--- js/script.js
+++ js/script.js
@@ -4,3 +4,33 @@ window.addEventListener('load', function(){
 
     faqBg.style.height = `${faqContainer.offsetHeight + 109}px`;
 });
+
+// Slider
+
+(function commentsSlider() {
+    const commentsBack = document.querySelector('#comments-back');
+    const commentsForward = document.querySelector('#comments-forward');
+    const commentsCards = document.querySelector('.comments__cards');
+
+    let leftOffset = 0;
+
+    commentsForward.addEventListener('click', function(){
+        leftOffset += 435;
+        if(leftOffset > 1300) {
+            commentsForward.style.opacity = '.5';
+            leftOffset = 0;
+        }
+        
+        commentsCards.style.left = `-${leftOffset}px`;
+    });
+
+    commentsBack.addEventListener('click', function(){
+        leftOffset -= 435;
+        if(leftOffset < 0) {
+            commentsBack.style.opacity = '.5';
+            leftOffset = 0;
+        }
+        commentsCards.style.left = `-${leftOffset}px`;
+    });
+
+}());
