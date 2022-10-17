2495b4cc434fdee6b39fc2890087aa723bbf061e
Ivan Kopytin <i.ivanzemsky2005@gmail.com>
2022-10-17 15:17:08 +0300

    Background of faq by position instead js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-window.addEventListener('load', function(){
-    const faqContainer = document.querySelector('.faq__wrap-container');
-    const faqBg = document.querySelector('.faq-bg');
-
-    faqBg.style.height = `${faqContainer.offsetHeight + 109}px`;
-});
-
 // Slider
 
 (function commentsSlider() {
