2d646ccc37ea4d1383910663e2fd01c6a6108e4c
Ivan Kopytin <i.ivanzemsky2005@gmail.com>
2022-10-28 17:33:43 +0300

    Slider adptive sizes & script

--- js/script.js
+++ js/script.js
@@ -1,27 +1,64 @@
+// lavelCancelFocus
+
+document.body.addEventListener('click', (event) => {
+    if (event.target.tagName == 'LABEL') {
+      event.preventDefault();
+    }
+  });
+
 // Slider
 
-(function commentsSlider() {
+(function() {
     const commentsBack = document.querySelector('#comments-back');
     const commentsForward = document.querySelector('#comments-forward');
     const commentsCards = document.querySelector('.comments__cards');
 
     let leftOffset = 0;
+    let leftOffsetStep = ((document.documentElement.clientWidth) / 5) + 110;
+    if (leftOffsetStep > 430) {
+        leftOffsetStep = 430;
+    }
+   
+    console.log(leftOffsetStep);
 
     commentsForward.addEventListener('click', function(){
-        leftOffset += 435;
-        if(leftOffset > 1300) {
-            leftOffset = 0;
+        if (leftOffset > -leftOffsetStep*2) {
+            leftOffset -= leftOffsetStep;
+            commentsCards.style.transform = `translateX(${leftOffset}px)`;
+            commentsBack.style.opacity = '1';
+
+            if (leftOffset <= -leftOffsetStep*2) {
+                commentsForward.style.opacity = '.5';
+            }
+    
         }
         
-        commentsCards.style.left = `-${leftOffset}px`;
     });
 
     commentsBack.addEventListener('click', function(){
-        leftOffset -= 435;
-        if(leftOffset < 0) {
-            leftOffset = 0;
+        if (leftOffset < 0) {
+            leftOffset += leftOffsetStep;
+            commentsCards.style.transform = `translateX(${leftOffset}px)`;
+            commentsForward.style.opacity = '1';
+
+            if (leftOffset == 0) {
+                commentsBack.style.opacity = '.5';
+            }
+    
         }
-        commentsCards.style.left = `-${leftOffset}px`;
+        
     });
 
 }());
+
+/* slider size
+
+(function() {
+    const commentsSlider = document.querySelector('.comments__slider');
+    const wrapperOffsetLeft = document.querySelector('.wrapper').offsetLeft;
+    
+    commentsSliderWidth = document.documentElement.clientWidth - 381 - wrapperOffsetLeft;
+    commentsSlider.style.width = `${commentsSliderWidth}px`;
+}());
+
+*/
